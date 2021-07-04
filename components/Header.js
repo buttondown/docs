e5f0992310faf67e2700a74e82a466164334560a
Justin Duke <justin@buttondown.email>
2021-07-04 16:58:09 -0700

    Make CTA an actual thing

--- components/Header.js
+++ components/Header.js
@@ -1,47 +1,47 @@
-import {
-    MenuAlt2Icon,
-    SearchIcon,
-  } from '@heroicons/react/outline'
-  
-export default function Header() { return (
+import { MenuAlt2Icon, SearchIcon } from "@heroicons/react/outline";
 
+export default function Header() {
+  return (
     <div className="relative z-10 flex-shrink-0 flex h-16 bg-white border-b border-gray-200">
-    <button
-      type="button"
-      className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900 lg:hidden"
-      onClick={() => setSidebarOpen(true)}
-    >
-      <span className="sr-only">Open sidebar</span>
-      <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
-    </button>
-    <div className="flex-1 px-4 flex justify-between">
-      <div className="flex-1 flex">
-        <form className="w-full flex lg:ml-0" action="#" method="GET">
-          <label htmlFor="search-field" className="sr-only">
-            Search
-          </label>
-          <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-            <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-              <SearchIcon className="h-5 w-5" aria-hidden="true" />
+      <button
+        type="button"
+        className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900 lg:hidden"
+        onClick={() => setSidebarOpen(true)}
+      >
+        <span className="sr-only">Open sidebar</span>
+        <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
+      </button>
+      <div className="flex-1 px-4 flex justify-between">
+        <div className="flex-1 flex">
+          <form className="w-full flex lg:ml-0" action="#" method="GET">
+            <label htmlFor="search-field" className="sr-only">
+              Search
+            </label>
+            <div className="relative w-full text-gray-400 focus-within:text-gray-600">
+              <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+                <SearchIcon className="h-5 w-5" aria-hidden="true" />
+              </div>
+              <input
+                id="search-field"
+                className="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
+                placeholder="Search"
+                type="search"
+                name="search"
+              />
             </div>
-            <input
-              id="search-field"
-              className="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-              placeholder="Search"
-              type="search"
-              name="search"
-            />
-          </div>
-        </form>
-      </div>
-      <div className="ml-4 flex items-center lg:ml-6">
-        <button
-          type="button"
-          className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-        >
-          Sign up for an account
-        </button>
+          </form>
+        </div>
+        <div className="ml-4 flex items-center lg:ml-6">
+          <a
+            href="https://buttondown.email/register"
+            target="_blank"
+            rel="noreferrer"
+            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+          >
+            Sign up for an account
+          </a>
+        </div>
       </div>
     </div>
-    </div>
-)}
+  );
+}
