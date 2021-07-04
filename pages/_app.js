8ab9935cd87750fbf10508fcdbdeb61b35ef1be9
Justin Duke <justin@buttondown.email>
2021-07-04 16:57:08 -0700

    Add Fathom support

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("FEFRJKHH", {
+      includedDomains: ["demo.buttondown.email"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  });
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default App;
