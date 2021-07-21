d4aa2a752ad15644f5ee385bb29806bcbb3542c9
Justin Duke <justin@buttondown.email>
2021-07-20 17:18:30 -0700

    Clean up search design

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -48,10 +48,13 @@ export default function Search(props: Props) {
     <>
       <div
         onClick={handleClick}
-        className="flex mx-2 px-2 py-1 border-2 rounded border-gray-400 font-weight-bold text-gray-300 bg-gray-700 text-sm font-semibold cursor-pointer"
+        className="flex mx-2 px-2 py-1 border-2 rounded-lg border-gray-400 font-weight-bold text-gray-300 bg-gray-700 text-sm font-semibold cursor-pointer items-center"
       >
         <SearchIcon className="h-5 w-4 mr-2" aria-hidden="true" />
-        Search
+        <div className="flex-1">Search</div>
+        <div className="font-mono bg-gray-500 rounded text-gray-100 px-1.5 text-xs">
+          /
+        </div>
       </div>
 
       <Transition appear show={props.searchOpen || false} as={Fragment}>
