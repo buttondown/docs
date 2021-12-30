bb7f8698ea6b313590729339e52905310c297551
Justin Duke <justin@buttondown.email>
2021-12-30 12:50:14 -0500

    Migrate to Tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
 module.exports = {
-  purge: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         "buttondown-blue": "#0069FF",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
