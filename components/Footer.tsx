3b5a99239eaefb2d8a339d2df3295ae267b766a5
sewingbuttondown <97639430+sewingbuttondown@users.noreply.github.com>
2022-02-26 22:37:53 -0800

    2022 now

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -91,7 +91,7 @@ export default function Footer() {
             ))}
           </div>
           <p className="mt-8 text-base text-gray-400 md:mt-0 md:order-1">
-            &copy; 2021 Buttondown, LLC. I hope you have a lovely day.
+            &copy; 2022 Buttondown, LLC. I hope you have a lovely day.
           </p>
         </div>
       </div>
