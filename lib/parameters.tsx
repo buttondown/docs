a418d3cedf58ad92a20aa3810405ce1884d0e7e8
Justin Duke <justin@buttondown.email>
2022-03-08 10:40:35 -0500

    [#545] Add result count to page parameter and DRY it up

---
+++ lib/parameters.tsx
@@ -0,0 +1,7 @@
+export const PAGE_PARAMETER =  {
+  parameter: "page",
+  type: "string",
+  description: "A page number within the paginated result set. Each page has 100 results.",
+  example: "",
+  optional: true,
+};
