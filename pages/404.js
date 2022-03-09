707dbed1cb5d4f68c7c82d996cc5da8656e8cd7b
Justin Duke <justin@buttondown.email>
2022-03-09 13:49:46 -0500

    [#546] Add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,6 @@
+import Layout from '../components/Layout';
+import { H1, P } from '../components/Markdown';
+
+export default function Custom404() {
+return <Layout><H1>Page not found</H1><p>Oh no — I can't find what you're looking for! Maybe check out something on the sidebar?</p></Layout>
+}
