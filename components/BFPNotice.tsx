753fe5e5a9df9d127a88725e598e8321a4a36d53
Justin Duke <justin@buttondown.email>
2022-10-31 11:18:44 -0400

    Remove references to BFP

--- components/BFPNotice.tsx
+++ components/BFPNotice.tsx
@@ -3,8 +3,8 @@ import Notice from "./Notice";
 export default function BFPNotice() {
   return (
     <Notice variant="info">
-      This section only applies to folks who have signed up for Buttondown for
-      Professionals.
+      This section only applies to folks who have signed up for one of
+      Buttondown's paid plans.
     </Notice>
   );
 }
