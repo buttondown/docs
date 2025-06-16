9d87f5fd20632b7273b2ea5e6b647bdbcf1d98cc
buttondown <engineering@buttondown.email>
2025-06-16 04:14:14 +0000

    Update from https://github.com/buttondown/monorepo/commit/dabac6d62401954b73b87e6c506273211805e3c0

--- components/iframe.tsx
+++ components/iframe.tsx
@@ -13,37 +13,39 @@ export default function Iframe({ src }: { src: string }) {
     src = url.toString();
   }
 
-  // Grab the path from the src.
-  const path = new URL(src).pathname;
+  // Grab the path from the src, and then replace all UUIDs with a placeholder.
+  const path = new URL(src).pathname.replace(
+    /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/g,
+    "<id>"
+  );
   return (
     <div>
-      <div className="relative p-8 -mx-8 bg-gray-100 pb-0 rounded-lg border border-gray-200 group hover:p-4! transition-all duration-300">
+      <div className="relative p-8 -mx-8 bg-gray-100 pb-0 pt-8 rounded-lg border border-gray-200 group hover:p-4! transition-all duration-300 overflow-hidden">
         <div className="bg-gray-100 rounded-t-lg rounded-b-none group-hover:rounded-b-lg shadow-md hover:shadow-2xl m-0 border border-gray-300 h-full flex flex-col transition-all duration-300">
-          <div className="rounded-t-lg bg-gray-200 px-4 py-1 flex items-center border-b border-gray-400 relative overflow-hidden">
-            <div className="absolute top-0 left-0 w-full h-full bg-gray-500/50 backdrop-blur-[1px] z-10" />
+          <div className="rounded-t-lg bg-gray-200 px-4 py-1 flex items-center border-b border-gray-400 relative max-w-full">
             <div className="flex gap-2">
               <div className="w-3 h-3 rounded-full bg-red-500" />
               <div className="w-3 h-3 rounded-full bg-yellow-500" />
               <div className="w-3 h-3 rounded-full bg-green-500" />
             </div>
-            <div className="flex-1 flex items-center justify-center ml-4">
-              <div className="bg-white px-4 py-1 w-full rounded-full text-xs my-0.5 border text-center border-gray-300 text-gray-600 flex justify-center items-center gap-2">
-                <div>
+            <div className="flex-1 flex items-center justify-center ml-4 max-w-full">
+              <div className="bg-white px-4 py-1 w-full rounded-full text-xs my-0.5 border text-center border-gray-300 text-gray-600 flex justify-center items-center gap-2  truncate overflow-x-clip break-words">
+                <div className="truncate break-words">
                   {EXTERNAL_DOMAIN}
                   {path}
                 </div>
               </div>
             </div>
             <a
-              href={`https://${EXTERNAL_DOMAIN}${path}`}
-              className="after:hidden! no-underline px-2 rounded-md hover:bg-gray-200 -mr-2 ml-2"
+              href={`https://${EXTERNAL_DOMAIN}${new URL(src).pathname}`}
+              className="after:hidden! no-underline px-2 rounded-md hover:bg-gray-200 -mr-2 ml-2 block flex-0"
             >
               ↗
             </a>
           </div>
           <iframe
             src={src.replace("demo.buttondown.com", EXTERNAL_DOMAIN)}
-            className="w-full aspect-video h-[300px] group-hover:h-[400px] transition-all duration-300 rounded-b-none group-hover:rounded-b-lg transition-all duration-300"
+            className="w-full aspect-video -mb-8 h-[calc(300px+2rem)] group-hover:h-[400px] transition-all duration-300 rounded-b-none group-hover:rounded-b-lg"
             title={src}
           />
         </div>
