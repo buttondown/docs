a39d9bd9310af54050b29ee87ebdc80b4cdf8081
buttondown <engineering@buttondown.email>
2025-12-23 00:00:08 +0000

    Update from https://github.com/buttondown/monorepo/commit/7acef7a5d6c1538db4d178fc6d538197e8b1789a

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,14 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { DESCRIPTION, TITLE } from "@/lib/constants";
 
+import "@fontsource/ibm-plex-mono/400.css";
+import "@fontsource/ibm-plex-mono/400-italic.css";
+import "@fontsource/ibm-plex-mono/500.css";
+import "@fontsource/ibm-plex-mono/500-italic.css";
+import "@fontsource/ibm-plex-mono/600.css";
+import "@fontsource/ibm-plex-mono/600-italic.css";
+import "@fontsource/ibm-plex-mono/700.css";
+import "@fontsource/ibm-plex-mono/700-italic.css";
 import "./globals.css";
 import Script from "next/script";
 
