966fce64d35b9c3074dbdcaa96aa70a7b8254f17
buttondown <engineering@buttondown.email>
2026-01-05 01:44:36 +0000

    Update from https://github.com/buttondown/monorepo/commit/ada8fc8eb59b58bb28a5e12abfb9601046113ed8

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,35 +1,25 @@
 import Link from "next/link";
-import AccountButtons from "../components/layout/account-buttons";
 import "./globals.css";
 
 export default function NotFound() {
-	return (
-		<html lang="en">
-			<body>
-				<div className="mb-8">
-					<AccountButtons />
-				</div>
-				<div className="container mx-auto p-8 flex items-center flex-col space-y-8 h-screen">
-					<div className="my-16 sm:my-32 text-center text-9xl">Alas!</div>
-
-					<div className="text-center space-y-4 m-auto">
-						<p className="inline-block text-base text-gray-500 bg-gray-200 px-2 py-1 rounded-md w-max">
-							404
-						</p>
-						<h1 className="text-xl sm:text-3xl font-extrabold text-gray-800 mb-8">
-							{" "}
-							Page not found
-						</h1>
-						<p className="text-base sm:text-lg">
-							Ruh roh, this page doesn’t exist.{" "}
-							<Link href="/" className="underline underline-offset-1">
-								Head home
-							</Link>
-							.
-						</p>
-					</div>
-				</div>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className="flex items-center justify-center h-screen">
+        <div className="container m-auto p-8 flex items-center flex-col space-y-8">
+          <div className="text-center font-bold text-9xl">Alas!</div>
+          <h1 className="text-xl sm:text-3xl font-extrabold mb-8">
+            {" "}
+            Page not found
+          </h1>
+          <p className="text-base sm:text-lg">
+            Ruh roh, this page doesn’t exist.{" "}
+            <Link href="/" className="underline underline-offset-1">
+              Head home
+            </Link>
+            .
+          </p>
+        </div>
+      </body>
+    </html>
+  );
 }
