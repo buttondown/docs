fd24cb976055d1cb81e7be52772f57cb51a21a31
buttondown <engineering@buttondown.email>
2026-01-08 22:12:59 +0000

    Update from https://github.com/buttondown/monorepo/commit/39a7b502cca603a51fa2f724a578922ee20c19aa

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,32 +13,33 @@ import "@fontsource/ibm-plex-mono/700-italic.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://docs.buttondown.com"),
-	title: TITLE,
-	description: DESCRIPTION,
-	openGraph: {
-		type: "website",
-		locale: "en_US",
-		siteName: TITLE,
-	},
+  metadataBase: new URL("https://docs.buttondown.com"),
+  title: TITLE,
+  description: DESCRIPTION,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    siteName: TITLE,
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body>
-				{children}
-				<Script
-					src="https://cdn.seline.com/seline.js"
-					data-token="fa13d3d8a7aeb56"
-					strategy="afterInteractive"
-				/>
-				<Script id="mermaid-fix" strategy="beforeInteractive">
-					{`
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Script
+          src="https://cdn.seline.com/seline.js"
+          data-token="fa13d3d8a7aeb56"
+          data-cookie="true"
+          strategy="afterInteractive"
+        />
+        <Script id="mermaid-fix" strategy="beforeInteractive">
+          {`
             // Fix a bug with mermaid block diagrams in react.
             //
             // relates to:
@@ -46,8 +47,8 @@ export default function RootLayout({
             // https://github.com/mermaid-js/mermaid/issues/5530
             (HTMLElement.prototype).toJSON = () => "";
           `}
-				</Script>
-			</body>
-		</html>
-	);
+        </Script>
+      </body>
+    </html>
+  );
 }
