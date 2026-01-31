c39bfff92582bd3d21071b14a3a39da3b1163753
buttondown <engineering@buttondown.email>
2026-01-31 11:07:12 +0000

    Update from https://github.com/buttondown/monorepo/commit/e1189afeb1b4700d6ace8b665a22d25cb0e64a84

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 export const DESCRIPTION =
   "Buttondown is the easiest way to start and grow your newsletter. Check out our docs and start building something great!";
-export const TITLE = "Buttondown Documentation";
+export const TITLE = "Buttondown Docs";
